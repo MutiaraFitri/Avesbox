0e1f3903bc561e973224747997207262f6e8a097
MutiaraFT <mutiarafitri99@mail.ugm.ac.id>
2020-11-19 20:05:15 +0700

    update

--- src/component/Cover.js
+++ src/component/Cover.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { Avatar, Layout, Menu, Icon, Button, Row, Col, Collapse, Input } from 'antd';
+import { Avatar, Layout, Menu, Icon, Button, Row, Col, Collapse, Input, Image } from 'antd';
 import logo from './../img/logo.png';
 import cover from './../img/cover-img.png';
 import panah from './../img/Vector.png';
 import './../../src/index.css';
+import './../../src/App.css';
+const { Content } = Layout;
 //import { ArrowRightOutlined } from 'antd';
 const { Header, Footer } = Layout;
 const { Panel } = Collapse;
@@ -12,94 +14,91 @@ const { Search } = Input;
 export class Cover extends Component {
     render() {
         return (
-            <div tyle={{ backgroundColor: "#fff" }}>
+
+            <div style={{ background: "linear-gradient(180deg, #F7DD5B 0%, #F1B347 114.44%)", height: "100vh" }}>
                 <Header style={{
                     backgroundColor: "rgba(192,192,192,0.0)",
                     height: "80px",
                     width: "100%",
-                    position: "absolute",
                     zIndex: 2
                 }}>
-                    <div className="logo" style={{ lineHeight: '40px', float: "left", color: "black", fontSize: "30px", marginTop: "30px" }}>
-                        <div style={{ fontSize: "28px", float: "left", marginRight: "10px" }}>
-                            <img src={logo} alt="logo" />
-                        </div>
-                        <div className="tulisan-logo" style={{ width: "600px" }}>
-                            <div style={{
-                                float: "left", fontSize: '39px', fontWeight: 800, margin: "0px",
-                            }}>
-                                AVESBOX
-                            </div>
-                            <div style={{ float: "left", fontSize: "22px", fontWeight: "normal", lineHeight: "36px", margin: "0px" }}>
-                                Layanan Otomatisati Kandang Unggas
-                            </div>
-                        </div>
-                    </div>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        style={{
-                            lineHeight: '74px',
-                            backgroundColor: "rgba(192,192,192,0.0)",
-                            float: "right",
-                            marginTop: "30px",
+                    <Row>
+                        <Col span={10} style={{ marginTop: "20px" }} >
+                            <Row>
+                                <div>
+                                    <Col span={4} style={{ float: "left", marginTop: "10px" }}>
+                                        <img src={logo} alt="logo" />
+                                    </Col>
+                                    <Col span={20} style={{ float: "left", fontSize: '39px', fontWeight: 800, margin: "0px", }}>
+                                        <div style={{
+                                            float: "left"
+                                        }}>
+                                            AVESBOX
+                                    </div>
+                                        <div style={{ float: "left", fontSize: "22px", lineHeight: "normal", fontWeight: "normal" }}>
+                                            Layanan Otomatisati Kandang Unggas
+                                    </div>
+                                    </Col>
+                                </div>
+                            </Row>
+                        </Col>
+                        <Col span={14} >
+                            <Menu
+                                theme="dark"
+                                mode="horizontal"
+                                style={{
+                                    lineHeight: '74px',
+                                    backgroundColor: "rgba(192,192,192,0.0)",
+                                    float: "right",
+                                    marginTop: "30px",
 
-                        }}
-                    >
-                        <Menu.Item key="1" style={{ fontSize: "18px", color: "#FFF", fontWeight: 500 }}>Home</Menu.Item>
-                        <Menu.Item key="2" style={{ fontSize: "18px", color: "#FFF", fontWeight: 500 }}>Produk</Menu.Item>
-                        <Menu.Item key="3" style={{ fontSize: "18px", color: "#FFF", fontWeight: 500 }}>Harga</Menu.Item>
-                        <Menu.Item key="4" style={{ fontSize: "18px", color: "#FFF", fontWeight: 500 }}>Blog</Menu.Item>
-                        <Menu.Item key="5" style={{ fontSize: "18px", color: "#FFF", fontWeight: 500 }}>Kontak Kami</Menu.Item>
-                        <Button size='large'
-                            style={{
-                                display: "absolute",
-                                top: "5px",
-                                borderRadius: "13px",
-                                height: "auto",
-                                backgroundColor: "#DB162F",
-                                color: "rgba(255,255,255,1)",
-                                border: "none",
-                                fontWeight: "bold",
-                                fontSize: "20px"
-                            }}>
-                            DAFTAR
+                                }}
+                            >
+                                <Menu.Item key="1" style={{ fontSize: "18px", color: "#000", fontWeight: 700, }}>Home</Menu.Item>
+                                <Menu.Item key="2" style={{ fontSize: "18px", color: "#000", fontWeight: 700, }}>Produk</Menu.Item>
+                                <Menu.Item key="3" style={{ fontSize: "18px", color: "#000", fontWeight: 700, }}>Harga</Menu.Item>
+                                <Menu.Item key="4" style={{ fontSize: "18px", color: "#000", fontWeight: 700, }}>Blog</Menu.Item>
+                                <Menu.Item key="5" style={{ fontSize: "18px", color: "#000", fontWeight: 700, }}>Kontak Kami</Menu.Item>
+                                <Button size='large'
+                                    style={{
+                                        display: "absolute",
+                                        top: "5px",
+                                        borderRadius: "10px",
+                                        height: "auto",
+                                        border: "2px solid #DB162F",
+                                        backgroundColor: "transparent",
+                                        color: "#000",
+                                        fontWeight: "bold",
+                                        fontSize: "20px"
+                                    }}>
+                                    DAFTAR
                         </Button>
-                    </Menu>
+                            </Menu>
+                        </Col>
+                    </Row>
+
                 </Header>
-                <div className="isi" style={{ fontSize: "50px", background: "linear-gradient(180deg, #F7DD5B 0%, #F1B347 114.44%)", height: 800, backgroundSize: "cover" }}>
-                    <div style={{ width: "100%", height: "100%", }}>
-                    </div>
-                    <div style={{ position: "absolute", zIndex: "2", top: "200px", width: "100%" }}>
-                        <Row>
-                            <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-                                <div style={{ float: "left", width: "400px", height: "350px", marginLeft: "62px", }}>
-                                    <img src={cover} alt="cover" />
-                                </div>
-                            </Col>
-                            <Col xs={2} sm={4} md={6} lg={8} xl={12}>
-                                <div style={{
-                                    width: "500px",
-                                    height: "173px",
-                                    position: "absolute",
-                                    top: "67px",
-                                    marginLeft: "250px",
-                                }}>
-                                    <div style={{ fontSize: "64px", fontWeight: 700, lineHeight: "70px" }}>
-                                        Mitra Kandang Unggas
+
+                <div className="isi" style={{ fontSize: "50px", marginTop: "90px", backgroundSize: "cover", padding: "0px 80px" }}>
+                    <Row>
+                        <Col span={14}>
+                            <Image
+                                width="90%"
+                                src={cover}
+                            />
+                        </Col>
+                        <Col span={10} style={{ paddingTop: "50px" }}>
+                            <div style={{ fontSize: "64px", fontWeight: 700, lineHeight: "70px" }}>
+                                Mitra Kandang Unggas
                                     </div>
-                                    <div style={{ fontSize: "29px", marginTop: "35px", fontWeight: 700, color: "#DB162F" }}>
-                                        Pelajari disini
+                            <div style={{ fontSize: "29px", marginTop: "35px", fontWeight: 700, color: "#DB162F" }}>
+                                Pelajari disini
                                         <img src={panah} alt="panah" style={{ marginLeft: "20px" }} />
-                                        { /* <ArrowRightOutlined /> */}
-                                    </div>
-                                    {/* <Button shape="round" icon="download" size="large" style={{ fontWeight: 700, color: "blue", textTransform: "uppercase", letterSpacing: "3px", fontSize: "14px" }}>
-                                        Download App
-                                    </Button> */}
-                                </div>
-                            </Col>
-                        </Row>
-                    </div>
+                                { /* <ArrowRightOutlined /> */}
+                            </div>
+                        </Col>
+                    </Row>
+
                 </div>
             </div >
         )
--- src/component/Intro.js
+++ src/component/Intro.js
@@ -3,46 +3,81 @@ import { Avatar, Layout, Menu, Icon, Button, Row, Col, Collapse, Input } from 'a
 import ayam from './../img/ayam.png';
 import './../../src/index.css';
 //import { ArrowRightOutlined } from 'antd';
-const { Header, Footer } = Layout;
+
+const { Content } = Layout;
 const { Panel } = Collapse;
 const { Search } = Input;
 
 export class Intro extends Component {
     render() {
         return (
-            <div tyle={{ fontFamily:"montserrat",backgroundColor: "#fff" }}>
-                <div className="isi" style={{ fontSize: "50px", background: "#F4EFEF", height: 800, backgroundSize: "cover" }}>
-                    <div style={{ width: "100%" }}>
-                        <Row>
-                            <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-                                <div style={{ float: "left", position: "absolute", zIndex: "1", }}>
-                                    <img src={ayam} alt="cover" style={{ marginTop: "470px" }} />
-                                </div>
-                            </Col>
-                            <Col xs={2} sm={4} md={6} lg={8} xl={24}>
-                                <div style={{
 
-                                    textAlign: "center",
-                                    margin: "0px auto",
-                                    marginTop: "250px",fontFamily:"montserrat",
-                                    float: "none", zIndex: "3",
-                                    width: "70%"
-                                }}>
-                                    <div style={{ fontSize: "34px", lineHeight: "70px" }}>
-                                        Apa itu <span style={{ fontWeight: 700 }}>AVES</span>Box ?
+            <Layout className="layout" style={{ fontFamily: "montserrat", backgroundColor: "#edebe5" }}>
+                <Content >
+                    <Row>
+                        <Col span={24}>
+                            <div style={{ float: "left", zIndex: "1", }}>
+                                <img src={ayam} alt="cover" style={{ bottom: "0px", position: "absolute", width: "400px " }} />
+                            </div>
+                            <div style={{
+
+                                textAlign: "center",
+                                margin: "0px auto",
+                                padding: "150px 0",
+                                fontFamily: "montserrat",
+                                float: "none", zIndex: "3",
+                                width: "70%"
+                            }}>
+                                <div style={{ fontSize: "34px", lineHeight: "70px" }}>
+                                    Apa itu <span style={{ fontWeight: 700 }}>AVES</span>Box ?
                                     </div>
-                                    <div style={{ fontSize: "29px", marginTop: "35px", color: "black" }}>
-                                        <p><span style={{ fontWeight: 700 }}>AVESBOX</span> adalah Layanan Otomasisasi Kandang Unggas yang dapat mengatasi masalah manajemen peternakan dengan menggunakan teknologi Internet of Things.</p>
-                                    </div>
-                                    {/* <Button shape="round" icon="download" size="large" style={{ fontWeight: 700, color: "blue", textTransform: "uppercase", letterSpacing: "3px", fontSize: "14px" }}>
+                                <div style={{ fontSize: "29px", marginTop: "35px", color: "black" }}>
+                                    <p><span style={{ fontWeight: 700 }}>AVESBOX</span> adalah Layanan Otomasisasi Kandang Unggas yang dapat mengatasi masalah manajemen peternakan dengan menggunakan teknologi Internet of Things.</p>
+                                </div>
+                                {/* <Button shape="round" icon="download" size="large" style={{ fontWeight: 700, color: "blue", textTransform: "uppercase", letterSpacing: "3px", fontSize: "14px" }}>
                                         Download App
                                     </Button> */}
-                                </div>
-                            </Col>
-                        </Row>
-                    </div>
-                </div>
-            </div >
+                            </div>
+                        </Col>
+                    </Row>
+                </Content>
+            </Layout>
+
+
+
+            // <div style={{ fontFamily: "montserrat", backgroundColor: "#fff" }}>
+            //     <div className="isi" style={{ fontSize: "50px", background: "#F4EFEF", height: 800, backgroundSize: "cover" }}>
+            //         <div style={{ width: "100%" }}>
+            //             <Row>
+            //                 <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+            //                     <div style={{ float: "left", position: "absolute", zIndex: "1", }}>
+            //                         <img src={ayam} alt="cover" style={{ marginTop: "470px" }} />
+            //                     </div>
+            //                 </Col>
+            //                 <Col xs={2} sm={4} md={6} lg={8} xl={24}>
+            // <div style={{
+
+            //     textAlign: "center",
+            //     margin: "0px auto",
+            //     marginTop: "250px", fontFamily: "montserrat",
+            //     float: "none", zIndex: "3",
+            //     width: "70%"
+            // }}>
+            //     <div style={{ fontSize: "34px", lineHeight: "70px" }}>
+            //         Apa itu <span style={{ fontWeight: 700 }}>AVES</span>Box ?
+            //     </div>
+            //     <div style={{ fontSize: "29px", marginTop: "35px", color: "black" }}>
+            //         <p><span style={{ fontWeight: 700 }}>AVESBOX</span> adalah Layanan Otomasisasi Kandang Unggas yang dapat mengatasi masalah manajemen peternakan dengan menggunakan teknologi Internet of Things.</p>
+            //     </div>
+            //     {/* <Button shape="round" icon="download" size="large" style={{ fontWeight: 700, color: "blue", textTransform: "uppercase", letterSpacing: "3px", fontSize: "14px" }}>
+            //         Download App
+            //     </Button> */}
+            // </div>
+            //                 </Col>
+            //             </Row>
+            //         </div>
+            //     </div>
+            // </div >
         )
     }
 }
